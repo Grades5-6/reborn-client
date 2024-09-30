bed069470e0a6fa29ed9a40db92efae8db84d09d
hwiwon <hwhwhw74@naver.com>
2024-10-01 00:42:46 +0900

    feat: kakao 로그인 인가 코드 #4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.browser)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,4 +71,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Retrofit 라이브러리
+    implementation ("com.squareup.retrofit2:retrofit:2.6.4")
+    // Gson 변환기 라이브러리
+    implementation ("com.squareup.retrofit2:converter-gson:2.6.4")
+    // Scalars 변환기 라이브러리
+    implementation ("com.squareup.retrofit2:converter-scalars:2.6.4")
+
 }
--- app/src/main/java/com/example/client/MainActivity.kt
+++ app/src/main/java/com/example/client/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.client
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.client.ui.theme.ClientTheme
@@ -19,11 +23,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    // 로그인 버튼
+                    Button(onClick = {
+                        // 버튼 클릭 시 OAuthRedirectActivity로 이동
+                        val intent = Intent(this@MainActivity, OAuthRedirectActivity::class.java)
+                        startActivity(intent)
+                    }) {
+                        Text("Kakao 로그인")
+                    }
                 }
             }
         }
