a2f29bb2d36a6b629779aa95f2707f64b823375d
hwidung <hwhwhw74@naver.com>
2024-12-06 15:45:38 +0900

    fix/#33 : base url 숨기기

--- app/src/main/java/com/example/client/data/api/RetrofitClient.kt
+++ app/src/main/java/com/example/client/data/api/RetrofitClient.kt
@@ -1,10 +1,11 @@
 package com.example.client.data.api
 
+import com.example.client.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private val BASE_URL = "https://re-born.asia/api/"
+    private const val BASE_URL = BuildConfig.BASE_URL
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
