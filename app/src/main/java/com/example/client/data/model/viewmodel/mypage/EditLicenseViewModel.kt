3d16319c5790ccc94c7ca60e0190a85d805ab009
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2024-12-07 01:27:08 +0900

    feat: EditLicense api 연동

--- app/src/main/java/com/example/client/data/model/viewmodel/mypage/EditLicenseViewModel.kt
+++ app/src/main/java/com/example/client/data/model/viewmodel/mypage/EditLicenseViewModel.kt
@@ -5,17 +5,33 @@ import androidx.lifecycle.viewModelScope
 import com.example.client.data.model.response.LicensesGetResponse
 import com.example.client.data.repository.mypage.EditLicenseRepository
 import com.example.client.domain.TestUserInfo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class EditLicenseViewModel(private val repository: EditLicenseRepository) : ViewModel() {
+    private val _selectedLicenses = MutableStateFlow<List<LicensesGetResponse>>(emptyList())
+    val selectedLicenses: StateFlow<List<LicensesGetResponse>> = _selectedLicenses
+
+    fun toggleLicencesSelection(license: LicensesGetResponse) {
+        val currentList = _selectedLicenses.value.toMutableList()
+        if (currentList.contains(license)) {
+            // 이미 선택된 자격증이면 제거
+            currentList.remove(license)
+        } else {
+            // 새로운 자격증이면 추가
+            currentList.add(license)
+        }
+        _selectedLicenses.value = currentList
+    }
 
     fun setUserLicenses(selectedLicenses: List<LicensesGetResponse>) {
         viewModelScope.launch {
             try {
                 val response: Response<Void> = repository.setUserLicense(selectedLicenses)
                 if (response.isSuccessful) {
-                    println("Interest fields updated successfully!")
+                    println("license fields updated successfully!")
                     TestUserInfo.LICENSES.clear()
                     TestUserInfo.LICENSES.addAll(selectedLicenses.map { license ->
                         LicensesGetResponse(
@@ -25,10 +41,10 @@ class EditLicenseViewModel(private val repository: EditLicenseRepository) : View
                         )
                     })
                 } else {
-                    println("Failed to update interest fields: ${response.code()}")
+                    println("Failed to update license fields: ${response.code()}")
                 }
             } catch (e: Exception) {
-                println("Error updating interest fields: ${e.message}")
+                println("Error updating license fields: ${e.message}")
             }
         }
     }
--- app/src/main/java/com/example/client/screen/mypage/EditLicenseScreen.kt
+++ app/src/main/java/com/example/client/screen/mypage/EditLicenseScreen.kt
@@ -1,13 +1,176 @@
 package com.example.client.screen.mypage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.client.R
+import com.example.client.component.all.ButtonColorEnum
+import com.example.client.component.all.ButtonComponent
+import com.example.client.component.all.CertificateComponent
+import com.example.client.data.model.viewmodel.SharedCertificationViewModel
 import com.example.client.data.model.viewmodel.mypage.EditLicenseViewModel
+import com.example.client.domain.TestUserInfo
 
 @Composable
 fun EditLicenseScreen(
+    sharedViewModel: SharedCertificationViewModel,
     editLicenseViewModel: EditLicenseViewModel,
     navController: NavController
 ){
+    var nickname by remember { mutableStateOf<String?>(null) }
 
+
+    val licensesState by sharedViewModel.licensesState.collectAsState()
+    val isLoading by sharedViewModel.isLoading.collectAsState()
+    val errorMessage by sharedViewModel.errorMessage.collectAsState()
+
+    val selectedLicenses by editLicenseViewModel.selectedLicenses.collectAsState()
+    val selectedLicensesCount = selectedLicenses.size
+
+    LaunchedEffect(Unit) {
+        nickname = TestUserInfo.TEST_USERNAME
+        sharedViewModel.fetchLicenses()
+    }
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFFFFEF4))
+    ) {
+        item {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(color = Color(0xFFFFFBDC)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Row(
+                    modifier = Modifier.padding(top = 30.dp),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "자격증 수정",
+                        style = TextStyle(
+                            fontSize = 24.sp,
+                            lineHeight = 33.6.sp,
+                            fontFamily = FontFamily(Font(R.font.pretendardextrabold)),
+                            color = Color(0xFF000000),
+                            textAlign = TextAlign.Center,
+                        )
+                    )
+                }
+            }
+        }
+
+        item {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 50.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    when {
+                        isLoading -> {
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+
+                        errorMessage != null -> {
+                            Text(
+                                text = errorMessage ?: "Unknown Error",
+                                color = Color.Red,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+
+                        licensesState.isNotEmpty() -> {
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(400.dp)
+                            ) {
+                                LazyColumn(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                ) {
+                                    items(licensesState) { license ->
+                                        CertificateComponent(
+                                            type = license.seriesnm,
+                                            name = license.jmfldnm,
+                                            date = license.expirationDate ?: "2024-12-05",
+                                            isSelected = if(TestUserInfo.LICENSES.contains(license)) true else false,
+                                            onItemSelected = { selectedLicense ->
+                                                editLicenseViewModel.toggleLicencesSelection(selectedLicense)
+                                            },
+                                            modifier = Modifier
+                                                .fillMaxSize()
+                                                .padding(horizontal = 39.dp),
+                                        )
+                                        Spacer(modifier = Modifier.height(25.dp))
+                                    }
+                                }
+                            }
+                        }
+
+                        else -> {
+                            Text(
+                                text = "No certificates available",
+                                modifier = Modifier.align(Alignment.Center),
+                                color = Color.Gray
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(20.dp))
+                ButtonComponent(
+                    buttonText = if (selectedLicensesCount == 0) "설정하기" else "${selectedLicensesCount}개 추가완료하기",
+                    buttonColorType = if (selectedLicensesCount == 0) ButtonColorEnum.LightGreen else ButtonColorEnum.Green,
+                    onClick = {
+                        editLicenseViewModel.setUserLicenses(selectedLicenses)
+                        navController.navigate("MyPage")
+                    },
+                )
+            }
+        }
+    }
 }
